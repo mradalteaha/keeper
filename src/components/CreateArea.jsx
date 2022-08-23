d962b47534271b6b1a285d7b94374bfc7e714317
moradte <moradte@ac.sce.ac.il>
2022-08-23 13:36:13 +0300

    adding missing files

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    const [note , noteUpdate] = useState({
+        title:"",
+        content:""
+    })
+
+    function handleChange(event){
+        
+        const {name , value } = event.target;
+
+        noteUpdate(prevNote => {
+            return {
+                ...prevNote ,
+                [name] : value
+               
+            }
+        })
+
+    }
+    function submitNote(event){
+
+        props.onAdd(note);
+
+        event.preventDefault();
+    }
+
+
+  return (
+    <div>
+      <form className="create-note">
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange}  value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
